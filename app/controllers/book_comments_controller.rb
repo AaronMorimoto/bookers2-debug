1c4299dbe72f8e648c36668831ff4b9a459ea0f4
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2022-12-29 09:38:22 +0000

    [comment Add]advanced3

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,27 @@
+class BookCommentsController < ApplicationController
+  
+  def create
+    #@book_comment = BookComment.new(user_id: current_user.id, book_id: params[:book_id])
+    #@book_comment.save
+    #redirect_to books_path(params[:book_id])
+    
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_to book_path(params[:book_id])
+  end
+  
+  def destroy
+    @book_comment = BookComment.find_by(user_id: current_user.id, book_id: params[:book_id])
+    @book_comment.destroy
+    redirect_to books_path(params[:book_id])
+  end
+  
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   
   has_one_attached :profile_image
 
@@ -17,4 +18,13 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+  
+  #def get_profile_image(10, 100)
+    #unless profile_image.attached?
+      #file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      #profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    #end
+    #profile_image.variant(resize_to_limit: [100, 100]).processed
+  #end
+  
 end
