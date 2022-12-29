444de1a9968ecbdb5e687ebcf41d07d380056e9c
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2022-12-29 10:43:30 +0000

    [comment Finish]advanced3

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,12 +19,4 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
   
-  #def get_profile_image(10, 100)
-    #unless profile_image.attached?
-      #file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-      #profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    #end
-    #profile_image.variant(resize_to_limit: [100, 100]).processed
-  #end
-  
 end
