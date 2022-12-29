41752c58db930352decdde1d3b1c7a896bc65226
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2022-12-29 06:42:08 +0000

    [favorite Finish]advanced3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
   def create
     @book_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
     @book_favorite.save
-    redirect_to book_path(paramms[:book_id])
+    redirect_to books_path(params[:book_id])
   end
   
   def destroy
     @book_favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
     @book_favorite.destroy
-    redirect_to book_path(paramms[:book_id])
+    redirect_to books_path(params[:book_id])
   end
   
 end
